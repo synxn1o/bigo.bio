f4e0a6f82d6ec9911f327b3118673944c04026e1
synxn1o <12211218@mail.sustech.edu.cn>
2026-05-24 23:40:22 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bigo.bio',
   output: 'server',
   adapter: cloudflare(),
+  integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
